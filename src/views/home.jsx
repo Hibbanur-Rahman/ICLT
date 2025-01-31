1ba19ea8430ba2095bef12eeb7b6780ef8dde8df
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2025-01-31 23:08:26 +0530

    adding the internal advisory boards

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -14,7 +14,8 @@ import {
   Send,
   Users,
 } from "lucide-react";
-
+import { VscLaw } from "react-icons/vsc";
+import qrCodeImg from "../assets/images/qrCodeImg.png";
 function Home() {
   const committees = {
     steeringCommittee: {
@@ -93,25 +94,73 @@ function Home() {
         },
       ],
     },
+    internalAdvisoryBoard: {
+      title: "Internal Advisory Board",
+      members: [
+        {
+          name: "Prof. Faizan Mustafa",
+          institution:
+            "Vice Chancellor, Chanakya National Law University, & Adjunct Professor, MANUU Law School",
+        },
+        {
+          name: "Prof. Tabrez Ahmad",
+          institution: "Dean& HOD, MANUU Law School",
+        },
+        {
+          name: "Dr. Sudhanshu Chandra",
+          institution: "Assistant Professor, MANUU Law School",
+        },
+        {
+          name: "Dr. Rashmi K S",
+          institution: "Assistant Professor, MANUU Law School",
+        },
+        {
+          name: "Dr. Sateesh Kumar",
+          institution: "Assistant Professor, MANUU Law School",
+        },
+        {
+          name: "Dr. Tausif Ur Rehman MD",
+          institution: "Assistant Professor, MANUU Law School",
+        },
+        {
+          name: "Mr. Mohammed Tabish",
+          institution: "Assistant Professor, MANUU Law School",
+        },
+      ],
+    },
   };
 
   return (
     <div className="min-h-screen bg-white flex flex-col justify-center items-center">
+      {/**===== header section ======== */}
       <header className="bg-gradient-to-r from-blue-900 to-blue-950 text-white py-20">
         <div className="container mx-auto px-4 text-center">
-          <p className="text-lg mb-4">Maulana Azad National Urdu University</p>
+          <p className="text-base mb-4">
+            Maulana Azad National Urdu University
+          </p>
           <p className="text-md mb-6">
             (A Central University, Hyderabad, India)
           </p>
+          <h1 className="text-lg font-semibold mb-4">MANUU LAW SCHOOL</h1>
           <h1 className="text-4xl md:text-6xl font-bold mb-4">
             INTERNATIONAL CONFERENCE ON LAW AND TECHNOLOGY
           </h1>
+
           <h2 className="text-2xl md:text-3xl mb-8">ICLT 2025</h2>
           <p className="text-xl mb-8 max-w-3xl mx-auto">
             Exploring the intricate relationship between law and technology in
             the digital age
           </p>
-          <Button size="lg" className="bg-white text-blue-900 hover:bg-blue-50" onClick={()=>window.open('https://docs.google.com/forms/d/e/1FAIpQLSfgyCBHjdUTIbUAbnQezpzDvkwZK0RdPZG4s0BSvbcGr5n3ew/viewform','_blank')}>
+          <Button
+            size="lg"
+            className="bg-white text-blue-900 hover:bg-blue-50"
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/forms/d/e/1FAIpQLSfgyCBHjdUTIbUAbnQezpzDvkwZK0RdPZG4s0BSvbcGr5n3ew/viewform",
+                "_blank"
+              )
+            }
+          >
             Register Now
           </Button>
         </div>
@@ -139,6 +188,37 @@ function Home() {
               university in research and education, National Assessment and
               Accreditation Council (NAAC), has awarded an 'A+' grade to the
               University.
+            </p>
+          </Card>
+        </div>
+      </section>
+      <section className="py-16 bg-white max-w-7xl">
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-center gap-2 mb-8">
+            <VscLaw className="w-8 h-8 text-blue-900" />
+            <h2 className="text-3xl font-bold text-center">About ICLT</h2>
+          </div>
+          <Card className="p-6">
+            <p className="text-gray-700 leading-relaxed">
+              Law & technology have an intricate relationship as they are
+              connected in a way which renders it challenging to decide whether
+              technology should come first or the other way around. Technology
+              has contributed to our advancement, creativity, & evolution from
+              the development of the wheel to the contemporary discoveries of
+              artificial intelligence. The way we live, work, and interact is
+              being completely transformed by new technologies including Robots,
+              Cybersecurity, Dgital media, communications, Artificial
+              Intelligence,Machine learning,Blockchain and Fintech. The legal
+              jurisprudence should evolve at the speed of technology,
+              specifically, on the issue of applicability & implication of
+              current laws and regulations that govern the use of technology
+              especially on issues concerning the existing
+              national/international law principles of state sovereignty, state
+              jurisdiction, state responsibility, & state liability. In order to
+              successfully negotiate the challenges of the digital age and
+              guarantee a peaceful coexistence that benefits society as a whole,
+              the conference seeks to promote a symbiotic relationship between
+              law & technology.
             </p>
           </Card>
         </div>
@@ -306,12 +386,27 @@ function Home() {
             </div>
           </div>
 
-          <div className="mb-12">
+          {/* <div className="mb-12">
             <h3 className="text-2xl font-semibold mb-6 text-blue-900">
               Publicity Chairs
             </h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               {committees.publicityChairs.members.map((member, index) => (
+                <Card key={index} className="p-6">
+                  <h4 className="text-xl font-bold">{member.name}</h4>
+                  <p className="text-gray-500 text-sm mt-1">
+                    {member.institution}
+                  </p>
+                </Card>
+              ))}
+            </div>
+          </div> */}
+          <div className="mb-12">
+            <h3 className="text-2xl font-semibold mb-6 text-blue-900">
+              Internal Advisory Board
+            </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {committees.internalAdvisoryBoard.members.map((member, index) => (
                 <Card key={index} className="p-6">
                   <h4 className="text-xl font-bold">{member.name}</h4>
                   <p className="text-gray-500 text-sm mt-1">
@@ -333,6 +428,63 @@ function Home() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <Card className="p-6 h-min">
+              <h3 className="text-xl font-bold mb-4 text-blue-900">
+                Guidelines for submission of abstract:
+              </h3>
+              <div className="space-y-4">
+                <h1 className="font-semibold">Title of the abstract</h1>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Author details with affiliation:
+                  </span>
+                  Name of the first author, corresponding author & co-authors,
+                  Institutional Affiliation, Correspondence Address, E-Mail ID
+                  and Phone Number.
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Abstract:
+                  </span>
+                  An abstract of about 350-500 words.
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Keywords:
+                  </span>
+                  Minimum 5 key words.
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Cover Page:
+                  </span>
+                  All the papers should be submitted with a cover page, if
+                  scholars/students should be forwarded through research
+                  supervisor/guide.
+                </p>
+              </div>
+
+              <div className="mt-6">
+                <h4 className="font-bold text-lg mb-4">Submission Procedure</h4>
+                <ol className="list-decimal list-inside space-y-2 text-gray-700">
+                  <li>
+                    Fill the google form on the link{" "}
+                    <span
+                      className="text-orange-400 underline cursor-pointer hover:text-blue-800"
+                      onClick={() =>
+                        window.open(
+                          "https://docs.google.com/forms/d/e/1FAIpQLSfgyCBHjdUTIbUAbnQezpzDvkwZK0RdPZG4s0BSvbcGr5n3ew/viewform",
+                          "_blank"
+                        )
+                      }
+                    >
+                      Iclt
+                    </span>
+                  </li>
+                  <li>Choose ICLT 2025 and submit your paper</li>
+                </ol>
+              </div>
+            </Card>
+            {/* <Card className="p-6 h-min">
               <h3 className="text-xl font-bold mb-4 text-blue-900">
                 Requirements
               </h3>
@@ -366,13 +518,13 @@ function Home() {
                   <li>Choose ICLT 2025 and submit your paper</li>
                 </ol>
               </div>
-            </Card>
+            </Card> */}
 
             <Card className="p-6">
               <h3 className="text-xl font-bold mb-4 text-blue-900">
-                Camera Ready Submission
+                Guidelines for submission of full length paper
               </h3>
-              <div className="space-y-4">
+              {/* <div className="space-y-4">
                 <p className="text-gray-700">
                   Please adhere to the following steps to prepare and submit
                   your camera-ready version by December 30, 2025.
@@ -430,9 +582,7 @@ function Home() {
                       Send the final version via email with:
                     </p>
                     <ul className="list-disc list-inside mt-2 space-y-1 text-gray-700">
-                      <li>
-                        Final PDF version (e.g., ICLT2025-paper-481.PDF)
-                      </li>
+                      <li>Final PDF version (e.g., ICLT2025-paper-481.PDF)</li>
                       <li>ZIP file with source files or Word document</li>
                     </ul>
                     <div className="mt-4 bg-blue-50 p-4 rounded-lg">
@@ -449,19 +599,161 @@ function Home() {
                           ICLT2025 Camera-Ready Submission [Paper ID]
                         </p>
                         <p>
-                          <span className="font-medium">Example:</span>{" "}
-                          ICLT2025 Camera-Ready Submission 481
+                          <span className="font-medium">Example:</span> ICLT2025
+                          Camera-Ready Submission 481
                         </p>
                       </div>
                     </div>
                   </div>
                 </div>
+              </div> */}
+              <div className="">
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Author details with affiliation:
+                  </span>
+                  Name of the first author, corresponding author & co-authors,
+                  Institutional Affiliation, Correspondence Address, E-Mail ID
+                  and Phone Number.
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Abstract:
+                  </span>
+                  An abstract of about 350-500 words with 5 keywords.
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Full Paper:
+                  </span>
+                  The word limit of the paper should be between 4000 to 6000
+                  words. The Editorial Board reserves the right to make changes
+                  to these conditions and guidelines mentioned herein.
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Main Text:
+                  </span>
+                  Times New Roman, font size 12, 1.5 spacing, justified, with a
+                  margin left 1.5 inch and right 1.0-inch, top 1 inch and bottom
+                  1 inch. The first line of the paragraph is not to be indented.
+                </p>
+                <h2 className="text-base font-medium"></h2>
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Sub-titles:
+                  </span>
+                  Sub titles in Times New Roman, font size 12, bold, capital
+                  letters, 1.5 line spacing.
+                </p>
+                {/* <h2 className="text-base font-medium">Citation:</h2> */}
+                <p className="text-base text-gray-600 my-2">
+                  <span className="text-base font-medium text-black me-2">
+                    Citation:
+                  </span>
+                  Submissions are required to strictly adhere to <br />
+                  <span className="font-semibold"> The Bluebook:</span>A Uniform
+                  Method of Citation, 20th Edition.
+                  <br />
+                  <span className="font-semibold">Similarity:</span> Should not
+                  exceed 10%
+                </p>
+                <p className="text-base text-gray-600 my-2">
+                  All papers shall include an 'Introduction' and 'Conclusion',
+                  all pages shall be numbered. All tables, charts, graphs,
+                  figures etc. should be kept to the minimum. They should be on
+                  separate sheets with sources indicated at the bottom.
+                </p>
+                <p className="text-orange-400 font-medium my-2">
+                  Submission to: MS Word file must be sent to iclt.law@manuu.ed
+                </p>
               </div>
             </Card>
           </div>
         </div>
       </section>
 
+      <section className="py-16 bg-gray-50 max-w-7xl">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-8">
+            Important Dates & payment details
+          </h2>
+          <div className="flex w-full gap-6">
+            <Card className="w-5/12">
+              <CardContent className="p-4">
+                <p className="text-base text-gray-800 my-3">
+                  <span className="font-semibold text-black text-lg me-2">
+                    Last date for submitting Abstract:
+                  </span>
+                  25th January,2025
+                </p>
+                <p className="text-base text-gray-800 my-3">
+                  <span className="font-semibold text-black text-lg me-2">
+                    Acceptance of Abstract:
+                  </span>
+                  27th January,2025
+                </p>
+                <p className="text-base text-gray-800 my-3">
+                  <span className="font-semibold text-black text-lg me-2">
+                    Last date for registration:
+                  </span>
+                  30th January,2025
+                </p>
+                <p className="text-base text-gray-800 my-3">
+                  <span className="font-semibold text-black text-lg me-2">
+                    Last date for submitting full length paper:
+                  </span>
+                  15th February 2025
+                </p>
+                <p className="text-base text-gray-800 my-3">
+                  <span className="font-semibold text-black text-lg me-2">
+                    Conference Date:
+                  </span>
+                  25th February 2025
+                </p>
+                <p className="text-base text-gray-800 my-3">
+                  <span className="font-semibold text-black text-lg me-2">Email:</span>
+                  iclt.law@manuu.edu.in
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="w-7/12">
+              <CardContent className="p-4 flex gap-[20px]">
+                <div className=" h-[300px] w-[300px] overflow-hidden rounded-xl border p-2">
+                  <img src={qrCodeImg} alt="" className=" h-full w-full" />
+                </div>
+                <div className="">
+                  <h1 className="text-lg font-bold"> Payment Details:</h1>
+                  <p className="text-base text-gray-600 my-2">
+                    <span className="text-base font-medium text-black me-2">
+                      Account Number:
+                    </span>
+                    187901000011247
+                  </p>
+                  <p className="text-base text-gray-600 my-2">
+                    <span className="text-base font-medium text-black me-2">
+                      Name (Account Holder):
+                    </span>
+                    Dean MANUU Law School
+                  </p>
+                  <p className="text-base text-gray-600 my-2">
+                    <span className="text-base font-medium text-black me-2">
+                      Bank Name:
+                    </span>
+                    Indian Overseas Bank
+                  </p>
+                  <p className="text-base text-gray-600 my-2">
+                    <span className="text-base font-medium text-black me-2">
+                      Branch:
+                    </span>
+                    1879 Gachibowli IFSC Code: IOBA0001879
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
       <section className="py-16 bg-gray-50 max-w-7xl">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">
